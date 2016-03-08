e0bf765ef1939f4c837ec5f72ed53bb80c4bfbda
kulakowka <kulakowka@gmail.com>
2016-03-09 00:22:19 +0300

    move search worker to separate npm package

--- workers/queues/search/index.js
+++ workers/queues/search/index.js
@@ -2,7 +2,7 @@
 
 var ref = require('../../utils/firebase')
 var client = require('../../utils/elasticsearch')
-var SearchWorker = require('./worker')
+var SearchWorker = require('firebase-elasticsearch-search-worker')
 
 var tutorialsSearchWorker = new SearchWorker({
   ref: ref.child('search/tutorials'),
